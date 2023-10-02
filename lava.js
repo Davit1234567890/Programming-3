d105e2a3456756dafb19048becf889808f31930e
Davit1234567890 <davit.h54@tumo.org>
2023-10-02 17:26:29 +0400

    popoxutyun 1

---
+++ lava.js
@@ -0,0 +1,82 @@
+class Lava {
+    constructor(x, y, index) {
+        this.x = x;
+        this.y = y;
+        this.index = index;
+        this.directions = [];
+    }
+    getNewCoordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+
+    chooseCell(character, character1, character2) {
+        this.getNewCoordinates()
+        var found = [];
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == character || matrix[y][x] == character1 || matrix[y][x] == character2) {
+                    found.push(this.directions[i]);
+                }
+            }
+        }
+        return found;
+    }
+    eat() {
+        let foods = this.chooseCell(1, 2, 3)
+        let food = random(foods)
+        if (food) {
+            matrix[this.y][this.x] = 5
+            let newX = food[0]
+            let newY = food[1]
+            matrix[food[1]][food[0]] = 5
+            this.x = newX
+            this.y = newY
+            for (var i in grassArr) {
+                if (newX == grassArr[i].x && newY == grassArr[i].y) {
+                    grassArr.splice(i, 1);
+                    break;
+                }
+            }
+            for (var i in grassEaterArr) {
+                if (newX == grassEaterArr[i].x && newY == grassEaterArr[i].y) {
+                    grassEaterArr.splice(i, 1);
+                    break;
+                }
+            }
+            for (var i in predatorArr) {
+                if (newX == predatorArr[i].x && newY == predatorArr[i].y) {
+                    predatorArr.splice(i, 1);
+                    break;
+                }
+            }
+        }
+        else {
+            this.move()
+        }
+
+
+    }
+    move() {
+        let emptyCells = this.chooseCell(0)
+        let newCell = random(emptyCells)
+        if (newCell) {
+            let newX = newCell[0]
+            let newY = newCell[1]
+            matrix[this.y][this.x] = 5
+            matrix[newY][newX] = 5
+            this.x = newX
+            this.y = newY
+        }
+    }
+}
---
+++ script.js
@@ -0,0 +1,113 @@
+var matrix = []
+
+var side = 60;
+var n = 50
+var m = 50
+
+for (let i = 0; i < n; i++) {
+    matrix.push([])
+    for (let j = 0; j < m; j++) {
+        matrix[i].push(0)
+    }
+}
+function characters(index, count) {
+    for (let a = 0; a < count; a++) {
+        var v = Math.floor(random(0, n))
+        var w = Math.floor(random(0, m))
+        matrix[v][w] = index
+    }
+}
+let grassArr = []
+let grassEaterArr = []
+let predatorArr = []
+let waterArr = []
+let lavaArr = []
+
+function setup() {
+    characters(1, 50)
+    characters(2, 20)
+    characters(3, 20)
+    characters(4, 1)
+    characters(5, 1)
+    frameRate(5);
+    createCanvas(matrix[0].length * side, matrix.length * side);
+    background('#acacac');
+
+    for (var y = 0; y < matrix.length; ++y) {
+        for (var x = 0; x < matrix[y].length; ++x) {
+            if (matrix[y][x] == 1) {
+                var gr = new Grass(x, y, 1);
+                grassArr.push(gr);
+            }
+            else if (matrix[y][x] == 2) {
+                var grEa = new GrassEater(x, y, 1);
+                grassEaterArr.push(grEa)
+            }
+            else if (matrix[y][x] == 3) {
+                var pre = new Predator(x, y, 1);
+                predatorArr.push(pre)
+            }
+            else if (matrix[y][x] == 4) {
+                var wat = new Water(x, y, 1)
+                waterArr.push(wat)
+            }
+            else if (matrix[y][x] == 5) {
+                var lav = new Lava(x, y, 1)
+                waterArr.push(lav)
+            }
+        }
+    }
+}
+
+function draw() {
+
+    for (var y = 0; y < matrix.length; y++) {
+        for (var x = 0; x < matrix[y].length; x++) {
+
+            if (matrix[y][x] == 1) {
+                fill("green");
+            }
+            else if (matrix[y][x] == 0) {
+                fill("#acacac");
+            }
+            else if (matrix[y][x] == 2) {
+                fill("yellow");
+            }
+            else if (matrix[y][x] == 3) {
+                fill("black");
+            }
+            else if (matrix[y][x] == 4) {
+                fill("blue")
+            }
+            else if (matrix[y][x] == 5) {
+                fill("orange")
+            }
+
+
+            rect(x * side, y * side, side, side);
+        }
+    }
+    for (var i in grassArr) {
+        grassArr[i].mul();
+    }
+    for (var i in grassEaterArr) {
+        grassEaterArr[i].eat()
+    }
+    for (var i in grassEaterArr) {
+        grassEaterArr[i].drink()
+    }
+    for (var i in predatorArr) {
+        predatorArr[i].eat()
+    }
+    for (var i in waterArr) {
+        waterArr[i].eat()
+    }
+    for (var i in lavaArr) {
+        lavaArr[i].eat()
+    }
+    for (var i in lavaArr) {
+        lavaArr[i].eat2()
+    }
+}
+
+console.log("test");
