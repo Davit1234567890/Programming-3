4dad652f07b93605098bb8fa0b7c1670db4478ab
Davit1234567890 <davit.h54@tumo.org>
2023-10-16 17:30:02 +0400

    popoxutyun 4

--- Grass.js
+++ Grass.js
@@ -16,3 +16,4 @@ class Grass extends LivingCreature {
         }
     }
 }
+
--- server.js
+++ server.js
@@ -1,16 +1,57 @@
-var express = require("express");
+var express = require('express');
 
 var app = express();
 
+var server = require('http').createServer(app);
+
+var io = require('socket.io')(server);
+
+var messages = [];
+
 app.use(express.static("."));
 
-app.get("/", function (req, res) {
+app.get('/', function (req, res) {
 
-    res.redirect("index.html");
+    res.redirect('index.html');
 
 });
 
-app.listen(3000, function () {
+server.listen(3000);
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var messages = [];
+app.use(express.static("."));
+app.get('/', function (req, res) {
+    res.redirect('index.html');
 
-    console.log("Example is running on port 3000");
 });
+
+server.listen(3000);
+
+function main() {
+    var socket = io();
+    var chatDiv = document.getElementById('chat');
+    var input = document.getElementById('message');
+    var button = document.getElementById('submit');
+    function handleSubmit() {
+        var val = input.value;
+        if (val != "") {
+            socket.emit("send message", val);
+        }
+        var p = document.createElement('p');
+
+p.innerText = msg;
+chatDiv.appendChild(p);
+input.value = "";
+}
+
+socket.on('display message', handleMessage);
+}  
+
+window.onload = main;
+
+    button.onclick = handleSubmit;
+
+    let random = require("./random");
